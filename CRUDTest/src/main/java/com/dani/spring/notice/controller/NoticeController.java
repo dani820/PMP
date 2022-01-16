83f3954b1f215466529b4b268222cc3d1b2b1c21
dani820 <danipark.st@gmail.com>
2022-01-16 22:23:48 +0900

    0116 1023 make notice table

--- CRUDTest/src/main/java/com/dani/spring/notice/controller/NoticeController.java
+++ CRUDTest/src/main/java/com/dani/spring/notice/controller/NoticeController.java
@@ -2,6 +2,8 @@ package com.dani.spring.notice.controller;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,23 +23,25 @@ public class NoticeController {
 	private NoticeService nService;
 
 	@RequestMapping (value="noticeList.di", method=RequestMethod.GET)
-	public ModelAndView noticeListView(ModelAndView mv, @RequestParam(value="page", required=false) Integer page) {
-		
-		int currentPage = (page != null) ? page : 1;
-		int listCount = nService.getListCount();
-		PageInfo pi = Pagination.getPageInfo(currentPage, listCount);
-		
-		ArrayList<Notice> nList = nService.printAll(pi);
-		
-		if(nList != null) {
-			mv.addObject("nList", nList);
-			mv.addObject("pi", pi);
-			mv.setViewName("notice/noticeList.jsp");
-		}else {
-			mv.addObject("msg", "공지사항 조회 실패");
-			mv.setViewName("errorPage");
-		}
-		
-		return mv;
+	public String noticeListView(HttpServletRequest request) {
+//	public ModelAndView noticeListView(ModelAndView mv, @RequestParam(value="page", required=false) Integer page) {
+//		
+//		int currentPage = (page != null) ? page : 1;
+//		int listCount = nService.getListCount();
+//		PageInfo pi = Pagination.getPageInfo(currentPage, listCount);
+//		
+//		ArrayList<Notice> nList = nService.printAll(pi);
+//		
+//		if(nList != null) {
+//			mv.addObject("nList", nList);
+//			mv.addObject("pi", pi);
+//			mv.setViewName("notice/noticeList.jsp");
+//		}else {
+//			mv.addObject("msg", "공지사항 조회 실패");
+//			mv.setViewName("errorPage");
+//		}
+//		
+//		return mv;
+		return "notice/noticeList";
 	}
 }
--- CRUDTest/src/main/webapp/WEB-INF/views/notice/noticeList.jsp
+++ CRUDTest/src/main/webapp/WEB-INF/views/notice/noticeList.jsp
@@ -1,12 +1,40 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<link rel="styleSheet" href="resources/css/notice/noticeList.css" type="text/css">
+<title>공지사항</title>
 </head>
 <body>
-
+	<jsp:include page="../menubar.jsp"/>
+	
+	<main>
+		<div class="frame">
+		<h1>공지사항</h1>
+			<%-- <c:if test="${!empty nList.size()}"> --%>
+				<table class="notice__table">
+					<thead>
+						<tr>
+							<td>No</td>
+							<td>제목</td>
+							<td>작성자</td>
+							<td>날짜</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>2</td>
+							<td>2</td>
+							<td>2</td>
+						</tr>
+					</tbody>
+				</table>
+			<%-- </c:if> --%>
+		</div>
+	</main>
 </body>
 </html>
